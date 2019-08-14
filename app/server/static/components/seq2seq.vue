69b30ca2ebdebded5eb1e926f3d23a50b3a207c8
Guillim <guigloo@msn.com>
2019-08-14 10:47:55 +0200

    cleaning a bit a file

--- app/server/static/components/seq2seq.vue
+++ app/server/static/components/seq2seq.vue
@@ -90,27 +90,7 @@ export default {
       this.annotations = questions
     },
 
-    addTodo() {
-      const value = this.newTodo && this.newTodo.trim();
-      if (!value) {
-        return;
-      }
-
-      const payload = {
-        text: value,
-      };
-
-      if (!this.annotations[this.pageNumber]) {
-        this.annotations[this.pageNumber] = []
-      }
-      // forcing the update for nested object
-      let questionCopy = JSON.parse(JSON.stringify(this.annotations))
-      questionCopy[this.pageNumber][this.currentQuestionIndex] = payload;
-      this.annotations = questionCopy
-      this.newTodo = '';
-    },
-
-    addTodoComplete() {
+    submitToDatabase() {
       const docId = this.docs[this.pageNumber].id;
       this.annotations[this.pageNumber].forEach((annotation) => {
         console.log('annotation:',JSON.parse(JSON.stringify(annotation)));
@@ -125,56 +105,12 @@ export default {
       console.log('FORCER A LA PAGE SUIVANTE');
     },
 
-    removeTodo(todo) {
-      const index = this.annotations[this.pageNumber].indexOf(todo);
-      this.annotations[this.pageNumber].splice(index, 1);
-    },
-
-    editTodo(todo) {
-      this.beforeEditCache = todo.text;
-      this.editedTodo = todo;
-    },
-
-    doneEdit(todo) {
-      if (!this.editedTodo) {
-        return;
-      }
-      this.editedTodo = null;
-      todo.text = todo.text.trim();
-      if (!todo.text) {
-        this.removeTodo(todo);
-      }
-      const docId = this.docs[this.pageNumber].id;
-
-      console.log('is this enough ?',response);
-    },
-
-    doneEditComplete(todo) {
-      // if (!this.editedTodo) {
-      //   return;
-      // }
-      // this.editedTodo = null;
-      // todo.text = todo.text.trim();
-      // if (!todo.text) {
-      //   this.removeTodo(todo);
-      // }
-      // const docId = this.docs[this.pageNumber].id;
-      // HTTP.put(`docs/${docId}/annotations/${todo.id}`, todo).then((response) => {
-      //   console.log(response); // eslint-disable-line no-console
-      // });
-    },
-
-    cancelEdit(todo) {
-      this.editedTodo = null;
-      todo.text = this.beforeEditCache;
-    },
-
-    async submit() {
-      const state = this.getState();
-      this.url = `docs?q=${this.searchQuery}&seq2seq_annotations__isnull=${state}&offset=${this.offset}`;
-      await this.search();
-      this.pageNumber = 0;
-    },
+    // async submit() {
+    //   const state = this.getState();
+    //   this.url = `docs?q=${this.searchQuery}&seq2seq_annotations__isnull=${state}&offset=${this.offset}`;
+    //   await this.search();
+    //   this.pageNumber = 0;
+    // },
 
     questionClass(i){
       let qNum = parseInt(this.currentNumberOfQuestion)
