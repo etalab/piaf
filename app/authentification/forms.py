8343b10395df4a19c39de667639485c7c3cf0543
Guillim <guigloo@msn.com>
2019-09-17 10:53:01 +0200

    we customise and translate the user signup form.
    And we remove the password confirmation - useless in my opinion

--- app/authentification/forms.py
+++ app/authentification/forms.py
@@ -6,7 +6,16 @@ User = get_user_model()
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
+    password1 = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
+
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1')
+        labels = {
+            'username': "Nom d'utilisateur",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop('password2')
