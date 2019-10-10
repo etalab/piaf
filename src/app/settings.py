b7110cd51e011db916de9d780f86b2fa2993b86d
Vincent Agnano <v.agnano@protonmail.com>
2019-10-10 15:29:21 +0200

    linted python

--- src/app/settings.py
+++ src/app/settings.py
@@ -1,4 +1,4 @@
-"""
+ """
 Django settings for app project.
 
 For more information on this file, see
@@ -24,21 +24,20 @@ BASE_PATH = Path(__file__).parent.parent
 BASE_DIR = str(BASE_PATH)
 
 env = Env()
-env.read_env(BASE_PATH / '.env', recurse=False)
+env.read_env(str(BASE_PATH / ".env"), recurse=False)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY',
-                 'v8sk33sy82!uw3ty=!jjv5vp7=s2phrzw(m(hrn^f7e_#1h2al')
+SECRET_KEY = env("SECRET_KEY", "v8sk33sy82!uw3ty=!jjv5vp7=s2phrzw(m(hrn^f7e_#1h2al")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', True)
+DEBUG = env.bool("DEBUG", True)
 
 # True if you want to allow users to be able to create an account
-ALLOW_SIGNUP = env.bool('ALLOW_SIGNUP', True)
+ALLOW_SIGNUP = env.bool("ALLOW_SIGNUP", True)
 
 # ALLOWED_HOSTS = []
 
@@ -46,131 +45,134 @@ ALLOW_SIGNUP = env.bool('ALLOW_SIGNUP', True)
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'server.apps.ServerConfig',
-    'api.apps.ApiConfig',
-    'widget_tweaks',
-    'rest_framework',
-    'django_filters',
-    'social_django',
-    'polymorphic',
-    'webpack_loader',
-    'anymail',
-    'piaf',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "server.apps.ServerConfig",
+    "api.apps.ApiConfig",
+    "widget_tweaks",
+    "rest_framework",
+    "django_filters",
+    "social_django",
+    "polymorphic",
+    "webpack_loader",
+    "anymail",
+    "piaf",
 ]
 
-CLOUD_BROWSER_APACHE_LIBCLOUD_PROVIDER = env('CLOUD_BROWSER_LIBCLOUD_PROVIDER', None)
-CLOUD_BROWSER_APACHE_LIBCLOUD_ACCOUNT = env('CLOUD_BROWSER_LIBCLOUD_ACCOUNT', None)
-CLOUD_BROWSER_APACHE_LIBCLOUD_SECRET_KEY = env('CLOUD_BROWSER_LIBCLOUD_KEY', None)
+CLOUD_BROWSER_APACHE_LIBCLOUD_PROVIDER = env("CLOUD_BROWSER_LIBCLOUD_PROVIDER", None)
+CLOUD_BROWSER_APACHE_LIBCLOUD_ACCOUNT = env("CLOUD_BROWSER_LIBCLOUD_ACCOUNT", None)
+CLOUD_BROWSER_APACHE_LIBCLOUD_SECRET_KEY = env("CLOUD_BROWSER_LIBCLOUD_KEY", None)
 
 if CLOUD_BROWSER_APACHE_LIBCLOUD_PROVIDER:
-    CLOUD_BROWSER_DATASTORE = 'ApacheLibcloud'
-    CLOUD_BROWSER_OBJECT_REDIRECT_URL = '/v1/cloud-upload'
-    INSTALLED_APPS.append('cloud_browser')
+    CLOUD_BROWSER_DATASTORE = "ApacheLibcloud"
+    CLOUD_BROWSER_OBJECT_REDIRECT_URL = "/v1/cloud-upload"
+    INSTALLED_APPS.append("cloud_browser")
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',
-    'applicationinsights.django.ApplicationInsightsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "social_django.middleware.SocialAuthExceptionMiddleware",
+    "applicationinsights.django.ApplicationInsightsMiddleware",
 ]
 
-ROOT_URLCONF = 'app.urls'
+ROOT_URLCONF = "app.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_PATH / 'server' / 'templates', BASE_PATH / 'authentification' / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            str(BASE_PATH / "server" / "templates"),
+            str(BASE_PATH / "authentification" / "templates"),
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
             ],
-            'libraries': {
-                'analytics': 'server.templatetags.analytics',
-                'utils_templating': 'authentification.templatetags.utils_templating',
+            "libraries": {
+                "analytics": "server.templatetags.analytics",
+                "utils_templating": "authentification.templatetags.utils_templating",
             },
         },
-    },
+    }
 ]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_PATH / 'staticfiles'
+STATIC_URL = "/static/"
+STATIC_ROOT = str(BASE_PATH / "staticfiles")
 
 STATICFILES_DIRS = [
     static_path
     for static_path in (
-        BASE_PATH / 'server' / 'static' / 'assets',
-        BASE_PATH / 'server' / 'static' / 'static',
+        str(BASE_PATH / "server" / "static" / "assets"),
+        str(BASE_PATH / "server" / "static" / "static"),
     )
     if path.isdir(static_path)
 ]
 
 
 WEBPACK_LOADER = {
-    'DEFAULT': {
-        'CACHE': not DEBUG,
-        'BUNDLE_DIR_NAME': 'bundle/',
-        'STATS_FILE': BASE_PATH / 'server' / 'static' / 'webpack-stats.json',
-        'POLL_INTERVAL': 0.1,
-        'TIMEOUT': None,
-        'IGNORE': [r'.*\.hot-update.js', r'.+\.map']
+    "DEFAULT": {
+        "CACHE": not DEBUG,
+        "BUNDLE_DIR_NAME": "bundle/",
+        "STATS_FILE": str(BASE_PATH / "server" / "static" / "webpack-stats.json"),
+        "POLL_INTERVAL": 0.1,
+        "TIMEOUT": None,
+        "IGNORE": [r".*\.hot-update.js", r".+\.map"],
     }
 }
 
-WSGI_APPLICATION = 'app.wsgi.application'
+WSGI_APPLICATION = "app.wsgi.application"
 
 AUTHENTICATION_BACKENDS = [
-    'social_core.backends.github.GithubOAuth2',
-    'django.contrib.auth.backends.ModelBackend',
+    "social_core.backends.github.GithubOAuth2",
+    "django.contrib.auth.backends.ModelBackend",
 ]
 
-SOCIAL_AUTH_GITHUB_KEY = env('OAUTH_GITHUB_KEY', None)
-SOCIAL_AUTH_GITHUB_SECRET = env('OAUTH_GITHUB_SECRET', None)
-GITHUB_ADMIN_ORG_NAME = env('GITHUB_ADMIN_ORG_NAME', None)
-GITHUB_ADMIN_TEAM_NAME = env('GITHUB_ADMIN_TEAM_NAME', None)
+SOCIAL_AUTH_GITHUB_KEY = env("OAUTH_GITHUB_KEY", None)
+SOCIAL_AUTH_GITHUB_SECRET = env("OAUTH_GITHUB_SECRET", None)
+GITHUB_ADMIN_ORG_NAME = env("GITHUB_ADMIN_ORG_NAME", None)
+GITHUB_ADMIN_TEAM_NAME = env("GITHUB_ADMIN_TEAM_NAME", None)
 
 if GITHUB_ADMIN_ORG_NAME and GITHUB_ADMIN_TEAM_NAME:
-    SOCIAL_AUTH_GITHUB_SCOPE = ['read:org']
+    SOCIAL_AUTH_GITHUB_SCOPE = ["read:org"]
 
 SOCIAL_AUTH_PIPELINE = [
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-    'server.social_auth.fetch_github_permissions',
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+    "server.social_auth.fetch_github_permissions",
 ]
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_PATH / 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": str(BASE_PATH / "db.sqlite3"),
     }
 }
 
@@ -180,43 +182,37 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 5,
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-    'SEARCH_PARAM': 'q',
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework_xml.renderers.XMLRenderer'
-    )
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 5,
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "SEARCH_PARAM": "q",
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        "rest_framework_xml.renderers.XMLRenderer",
+    ),
 }
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -224,35 +220,39 @@ USE_L10N = True
 
 USE_TZ = True
 
-TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
-TEST_OUTPUT_DIR = BASE_PATH / 'junitxml'
+TEST_RUNNER = "xmlrunner.extra.djangotestrunner.XMLTestRunner"
+TEST_OUTPUT_DIR = str(BASE_PATH / "junitxml")
 
-LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/projects/1/'
-LOGOUT_REDIRECT_URL = '/projects/1/'
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/projects/1/"
+LOGOUT_REDIRECT_URL = "/projects/1/"
 
 django_heroku.settings(locals(), test_runner=False)
 
 # Change 'default' database configuration with $DATABASE_URL.
-DATABASES['default'].update(dj_database_url.config(
-    env='DATABASE_URL',
-    conn_max_age=env.int('DATABASE_CONN_MAX_AGE', 500),
-    ssl_require='sslmode' not in furl(env('DATABASE_URL', '')).args,
-))
+DATABASES["default"].update(
+    dj_database_url.config(
+        env="DATABASE_URL",
+        conn_max_age=env.int("DATABASE_CONN_MAX_AGE", 500),
+        ssl_require="sslmode" not in furl(env("DATABASE_URL", "")).args,
+    )
+)
 
 # work-around for dj-database-url: explicitly disable ssl for sqlite
-if DATABASES['default'].get('ENGINE') == 'django.db.backends.sqlite3':
-    DATABASES['default'].get('OPTIONS', {}).pop('sslmode', None)
+if DATABASES["default"].get("ENGINE") == "django.db.backends.sqlite3":
+    DATABASES["default"].get("OPTIONS", {}).pop("sslmode", None)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Allow all host headers
 # ALLOWED_HOSTS = ['*']
 
 # Size of the batch for creating documents
 # on the import phase
-IMPORT_BATCH_SIZE = env.int('IMPORT_BATCH_SIZE', 500)
+IMPORT_BATCH_SIZE = env.int("IMPORT_BATCH_SIZE", 500)
+
+GOOGLE_TRACKING_ID = env("GOOGLE_TRACKING_ID", "")
 
 ## necessary for email verification setup
 # EMAIL_USE_TLS = True
@@ -262,40 +262,31 @@ IMPORT_BATCH_SIZE = env.int('IMPORT_BATCH_SIZE', 500)
 # EMAIL_PORT = 587
 #
 
-MAILJET_API_KEY = env('MAILJET_API_KEY', None)
-MAILJET_SECRET_KEY = env('MAILJET_SECRET_KEY', None)
-USE_MAILJET = env.bool('USE_MAILJET', False)
+MAILJET_API_KEY = env("MAILJET_API_KEY", None)
+MAILJET_SECRET_KEY = env("MAILJET_SECRET_KEY", None)
+USE_MAILJET = env.bool("USE_MAILJET", False)
 DEFAULT_FROM_EMAIL = "piaf@data.gouv.fr"
 
 MATOMO_SITE_ID = env("MATOMO_SITE_ID", "")
 
 # information here: https://anymail.readthedocs.io/en/stable/esps/mailjet/
-ANYMAIL = {
-    "MAILJET_API_KEY": MAILJET_API_KEY,
-    "MAILJET_SECRET_KEY": MAILJET_SECRET_KEY,
-}
+ANYMAIL = {"MAILJET_API_KEY": MAILJET_API_KEY, "MAILJET_SECRET_KEY": MAILJET_SECRET_KEY}
 
 if USE_MAILJET:
-    EMAIL_BACKEND = 'anymail.backends.mailjet.EmailBackend'
+    EMAIL_BACKEND = "anymail.backends.mailjet.EmailBackend"
 else:
     ## During development only
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': BASE_PATH / 'debug.log',
-        },
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": str(BASE_PATH / "debug.log"),
+        }
     },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
+    "loggers": {"django": {"handlers": ["file"], "level": "ERROR", "propagate": True}},
 }
