4800094fddaa9f3d37bc4b7e1447d0528e9084f7
Vincent Agnano <v.agnano@protonmail.com>
2019-10-10 15:53:23 +0200

    save question/answers

---
+++ src/piaf/tests.py
@@ -0,0 +1,45 @@
+from django.test import Client, TestCase
+
+from django.contrib.auth.models import User
+from .models import Article, Paragraph, ParagraphBatch
+
+client = Client()
+
+
+class ApiTest(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='user')
+        user.set_password('password')
+        user.save()
+        self.user = client.login(username='user', password='password')
+        self.article = Article(name="My First Article")
+        self.article.save()
+        self.batch = ParagraphBatch()
+        self.batch.save()
+        self.paragraphs = []
+        for i in range(0, 5):
+            paragraph = Paragraph(
+                text=f"this is text {i + 1}", article=self.article, batch=self.batch
+            )
+            paragraph.save()
+            self.paragraphs.append(paragraph)
+
+    def test_post_annotation(self):
+        client.post(
+            "/app/api/article",
+            content_type="application/json",
+            data={
+                "paragraph": self.paragraphs[0].pk,
+                "data": [
+                    {"question": "q1", "answer": {"text": "a1", "index": 11}},
+                    {"question": "q2", "answer": {"text": "a2", "index": 12}},
+                    {"question": "q3", "answer": {"text": "a3", "index": 13}},
+                    {"question": "q4", "answer": {"text": "a4", "index": 14}},
+                    {"question": "q5", "answer": {"text": "a5", "index": 15}},
+                ],
+            },
+        )
+
+        article = Article.objects.first()
+        self.assertEqual(article.paragraphs.first().questions.count(), 5)
+        self.assertEqual(article.paragraphs.first().questions.last().answers.first().text, "a5")
--- src/piaf/views.py
+++ src/piaf/views.py
@@ -2,9 +2,12 @@ import json
 from random import randint
 
 from django.views.generic import TemplateView, View
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.forms.models import model_to_dict
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 from api.permissions import SuperUserMixin
 from .models import Article, ParagraphBatch, Paragraph, Question, Answer
@@ -40,10 +43,25 @@ class AdminView(TemplateView, SuperUserMixin):
 
 class ArticleApi(View):
     # Provide a randomly picked pending article.
-    def dispatch(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         qs = ParagraphBatch.objects.filter(status="pending")
         batch = qs[randint(0, qs.count() - 1)]
         paragraphs = Paragraph.objects.filter(batch=batch)
         data = model_to_dict(batch.article, ("name",))
         data["paragraphs"] = [model_to_dict(p, ("text",)) for p in paragraphs]
         return HttpResponse(json.dumps(data), content_type="application/json")
+
+    @method_decorator(csrf_exempt)
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        paragraph = Paragraph.objects.get(pk=data["paragraph"])
+        for qa in data["data"]:
+            question = Question(paragraph=paragraph, text=qa["question"])
+            question.save()
+            answer = Answer(
+                question=question,
+                text=qa["answer"]["text"],
+                start_position=qa["answer"]["index"],
+            )
+            answer.save()
+        return HttpResponse(status=201)
