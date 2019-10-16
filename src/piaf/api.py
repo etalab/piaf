5b1cc4f90a8e974d866e9f767b317bd2e28e14e9
Vincent Agnano <v.agnano@protonmail.com>
2019-10-16 17:05:11 +0200

    reorganized piaf views and apis

--- src/piaf/api.py
+++ src/piaf/api.py
@@ -1,9 +1,13 @@
+from random import randint
+import json
+
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_polymorphic.serializers import PolymorphicSerializer
+
+from .models import ParagraphBatch, Paragraph
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -21,8 +25,35 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class MeView(APIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         serializer = UserSerializer(request.user, context={"request": request})
         return Response(serializer.data)
+
+
+class ParagraphView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    # Provide a randomly picked pending article.
+    def get(self, request, *args, **kwargs):
+        theme = request.GET.get("theme")
+        qs = ParagraphBatch.objects.filter(status="pending")
+        if theme:
+            qs = qs.filter(paragraphs__article__theme=theme)
+        batch = qs[randint(0, qs.count() - 1)]
+        article = batch.article
+        paragraph = batch.paragraphs.filter(status="pending").first()
+        data = {
+            "id": paragraph.id,
+            "theme": article.theme,
+            "text": paragraph.text,
+            "title": article.name,
+        }
+        return Response(data)
+
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        paragraph = Paragraph.objects.get(pk=data["paragraph"])
+        paragraph.complete(data["data"], request.user)
+        return Response(status=201)
--- src/piaf/urls.py
+++ src/piaf/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import IndexView, ParagraphApi, AdminView
-from .api import MeView
+from .views import IndexView, AdminView
+from .api import MeView, ParagraphView
 
 
 app_name = "app"
@@ -9,6 +9,6 @@ app_name = "app"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("admin", AdminView.as_view(), name="admin"),
-    path("api/paragraph", ParagraphApi.as_view(), name="api_paragraph"),
+    path("api/paragraph", ParagraphView.as_view(), name="api_paragraph"),
     path("me", MeView.as_view(), name="me"),
 ]
