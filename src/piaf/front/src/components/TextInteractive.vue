bf2084e7e28fdae1f193d67a1c34e9ed92e7d759
Guillim <guigloo@msn.com>
2019-10-23 20:15:43 +0200

    setp 1: cleaning and new format

--- src/piaf/front/src/components/TextInteractive.vue
+++ src/piaf/front/src/components/TextInteractive.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="alignLeft">
+  <div class="alignLeft" id="paragraph">
     <span
-      oncopy="return false"
-      oncut="return false"
-      v-for="r in chunks"
-      v-bind:style="getChunkStyle(r)"
-      v-touch:tap="setSelectedRange"
-      v-touch:start="setSelectedRange"
-      v-touch:end="setSelectedRange"
-      v-touch:moving="setSelectedRange"
-    >{{ textPart(r) }}<button v-if="r.label==1" v-on:click="removeAnswer()" class="removeBtn">
-       <v-icon fab small dark size=12 >mdi-close</v-icon>
-     </button>
+    oncopy="return false"
+    oncut="return false"
+    v-touch:tap="setSelectedRange"
+    v-touch:start="setSelectedRange"
+    v-touch:end="setSelectedRange"
+    v-touch:moving="setSelectedRange"
+    >
+    {{this.currentDocument.text}}
     </span>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import SelectText from '@vinyll/selecttext'
 
 export default {
   data: () => ({
@@ -27,58 +25,7 @@ export default {
   computed: {
     ...mapState([
       'currentDocument',
-      'annotations',
-      'currentQuestionIndex'
     ]),
-    entityPositionsOrempty () {
-      return this.$store.getters.answersForTextInteraction
-    },
-    // entityPositions() {
-    //   // return [this.annotations[this.currentQuestionIndex].answer]
-    //   return this.annotations
-    //   .map(annotation => annotation.answer)
-    //   .filter(answer => typeof answer === 'object')
-    // },
-    // entityPositionsOrempty() {
-    //   return (this.editMode) ? [] : this.entityPositions;
-    // },
-
-    // sortedEntityPositions() {
-    //   let answers = this.entityPositionsOrempty.map(anno => {anno.isCurrentAnswer = 0; return anno})
-    //   answers[this.currentQuestionIndex].isCurrentAnswer = 1
-    //   return answers.sort((a, b) => a.index - b.index);
-    // },
-
-    sortedEntityPositions() {
-      let arr = this.entityPositionsOrempty[this.currentQuestionIndex]
-      arr.isCurrentAnswer = 1
-      return [arr]
-    },
-
-    chunks() {
-      const res = [];
-      let left = 0;
-      let e;
-      for (let i = 0; i < this.sortedEntityPositions.length; i++) {
-        e = this.sortedEntityPositions[i];
-        if (e.text) {
-          e.label = (e.isCurrentAnswer === 1) ? 1 : 2;
-          e.startOffset = e.index
-          e.endOffset = e.startOffset + e.text.length
-
-          const l = this.makeEmptyChunk(left, e.startOffset);
-
-          res.push(l);
-          res.push(e);
-          left = e.endOffset;
-        }
-      }
-      const l = this.makeEmptyChunk(left, this.currentDocument.text.length);
-      res.push(l);
-
-      return res;
-    },
-
   },
 
   methods: {
@@ -112,23 +59,21 @@ export default {
       let start;
       let end;
       let text;
-      if (window.getSelection) {
-        const range = window.getSelection().getRangeAt(0);
-        const preSelectionRange = range.cloneRange();
-        preSelectionRange.selectNodeContents(this.$el);
-        preSelectionRange.setEnd(range.startContainer, range.startOffset);
-        start = [...preSelectionRange.toString()].length;
-        end = start + [...range.toString()].length;
-        text = range.toString()
-      } else if (document.selection && document.selection.type !== 'Control') {
-        const selectedTextRange = document.selection.createRange();
-        const preSelectionTextRange = document.body.createTextRange();
-        preSelectionTextRange.moveToElementText(this.$el);
-        preSelectionTextRange.setEndPoint('EndToStart', selectedTextRange);
-        start = [...preSelectionTextRange.text].length;
-        end = start + [...selectedTextRange.text].length;
-        text = selectedTextRange.text
+
+      const container = document.querySelector('#paragraph')
+      //this.$el
+      const selector = new SelectText(container)
+      selector.onSelect = (range) => {
+        console.log('range',range);
+        if (range.length) {
+          start = range[0]
+          end = range[1]
+          text = container.textContent.substr(range[0], range[1] - range[0] + 1)
+        } else {
+          console.log('not yet defined');
+        }
       }
+
       this.$store.commit('setStartOffset', start)
       this.$store.commit('setEndOffset', end)
       this.$store.commit('setHighlitedText', text)
@@ -136,16 +81,6 @@ export default {
 
     textPart(r) {
       return [...this.currentDocument.text].slice(r.startOffset, r.endOffset).join('');
-    },
-
-    makeEmptyChunk(startOffset, endOffset) {
-      const chunk = {
-        id: 0,
-        label: 0,
-        startOffset: startOffset,
-        endOffset: endOffset,
-      };
-      return chunk;
     },
 
     removeAnswer(){
@@ -173,4 +108,27 @@ export default {
 .removeBtn i{
   font-size: 12px;
 }
+</style>
+
+<style>
+#paragraph span {
+  cursor: pointer;
+  user-select: none;
+}
+#paragraph span:hover {
+  background-color: #d4e6ff;
+}
+#paragraph span.selected.first {
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+}
+#paragraph span.selected.last {
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+}
+#paragraph span.selected {
+  color: #ffffff;
+  background-color: #4169e1;
+}
+
 </style>
