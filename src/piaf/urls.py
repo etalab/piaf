cb12387e40d8ca0269544c53c3d586585164c244
Vincent Agnano <v.agnano@protonmail.com>
2019-10-29 14:59:50 +0100

    download results as json

--- src/piaf/urls.py
+++ src/piaf/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import IndexView, AdminView, ParagraphView
+from .views import IndexView, AdminView, DatasetView, ParagraphView
 from .apis import MeView
 
 
@@ -9,6 +9,7 @@ app_name = "app"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("admin", AdminView.as_view(), name="admin"),
+    path("admin/dataset", DatasetView.as_view(), name="dataset"),
     path("api/paragraph", ParagraphView.as_view(), name="api_paragraph"),
     path("me", MeView.as_view(), name="me"),
 ]
--- src/piaf/views.py
+++ src/piaf/views.py
@@ -3,6 +3,7 @@ from random import randint
 
 from django.views.generic import TemplateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms.models import model_to_dict
 from django.http import HttpResponse, JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -17,7 +18,46 @@ class IndexView(LoginRequiredMixin, TemplateView):
 
 class AdminView(SuperUserMixin, TemplateView):
     template_name = "piaf/admin.html"
+
+
+class DatasetView(SuperUserMixin, TemplateView):
+    template_name = "piaf/admin.html"
     count_inserted_articles = None
+
+    def get(self, request, *args, **kwargs):
+        data = []
+        for article in Article.objects.filter(
+            paragraphs__status="completed"
+        ).prefetch_related("paragraphs__questions__answers"):
+            paragraphs = []
+            for paragraph in article.paragraphs.filter(status="completed"):
+                questions = []
+                for question in paragraph.questions.all():
+                    q = {
+                        "text": question.text,
+                        "answers": [
+                            model_to_dict(a, ["text", "index"])
+                            for a in question.answers.all()
+                        ],
+                    }
+                    questions.append(q)
+                p = {"text": paragraph.text, "questions": questions}
+                paragraphs.append(p)
+            d = {
+                "displaytitle": article.name,
+                "categorie": article.theme,
+                "wikipedia_page_id": article.reference,
+                "audience": article.audience,
+                "paragraphs": paragraphs,
+            }
+            data.append(d)
+            response = HttpResponse(
+                json.dumps(list(data)), content_type="application/force-download"
+            )
+            response[
+                "Content-Disposition"
+            ] = "attachment; filename=piaf-annotations.json"
+            return response
 
     def post(self, request, *args, **kwargs):
         content = request.FILES["file"].read()
@@ -35,8 +75,8 @@ class AdminView(SuperUserMixin, TemplateView):
                     batch = ParagraphBatch()
                     batch.save()
                 Paragraph(batch=batch, article=article, text=p["context"]).save()
-        self.count_inserted_articles = len(data)
-        return self.get(request, *args, **kwargs)
+        context = {"count_inserted_articles": len(data)}
+        return self.render_to_response(context=context)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -46,9 +86,8 @@ class AdminView(SuperUserMixin, TemplateView):
 
 @method_decorator(csrf_exempt, name="dispatch")
 class ParagraphView(View):
-
-    # Provide a randomly picked pending article.
     def get(self, request, *args, **kwargs):
+        """Provide a randomly picked pending article."""
         theme = request.GET.get("theme")
         qs = ParagraphBatch.objects.exclude(status="completed")
         # Limit display by audience
