074b94e66024c3ce898571be40ef905a707a715a
Guillim <guigloo@msn.com>
2020-01-30 11:53:31 +0100

    we add the wording from Benjamin

--- src/piaf/front/src/components/TextTitle.vue
+++ src/piaf/front/src/components/TextTitle.vue
@@ -10,19 +10,25 @@
         <v-icon fab small dark class="black--text ml-1" >mdi-information-outline</v-icon>
       </span>
     </template>
-    <span><strong>"{{currentDocument.title}}"</strong> est le titre de l'article Wikipédia dont est extrait ce paragraphe.
-      <span v-if="currentDocumentChapitresInfo.total == 1">
-        Cet article est composé de {{currentDocument.count_pending_paragraphs + currentDocument.count_completed_paragraphs}} paragraphes, et vous en êtes au numéro  {{currentDocument.count_completed_paragraphs + 1}} </strong>.
-      </span>
-      <span v-else>
-        Cet article est découpé en <strong>{{currentDocumentChapitresInfo.total}} chapitres, et vous en êtes au numéro {{currentDocumentChapitresInfo.total - currentDocumentChapitresInfo.toDo + 1}}</strong>. Dans ce chapitre, vous en êtes au <strong>paragraphe {{currentDocument.count_completed_paragraphs + 1}} / {{currentDocument.count_pending_paragraphs + currentDocument.count_completed_paragraphs}}</strong>.
-      </span>
-    <br><br>
-    <strong>Qu'est-ce qu'un chapitre ?</strong><br>
-    Pour rendre le travail d'annotation plus agréable, nous avons regroupé les paragraphes en groupe de 5 : les chapitres. Par exemple, un article wikipedia de 8 paragraphes sera découpé en deux chapitres : le premier de 5 paragraphes, et le second de 3 paragraphes.
-    <br><br>
-    <strong>Combien de question faut-il par paragraphe ?</strong><br>
-    Pour chaque paragraphe, il faudra trouver 5 questions-réponses.
+    <span><strong>"{{currentDocument.title}}"</strong> est le titre de l'article Wikipédia dont est issu le paragraphe ci-dessous que vous allez annoter.<br>
+      <strong>Pourquoi mon article wikipédia est-il divisé en paragraphe ?</strong><br>
+      Afin de respecter le protocole scientifique de PIAF ainsi que pour faciliter l’annotation, chaque article wikipédia a été divisé en paragraphe. Chaque paragraphe est numéroté selon sa position dans l’article.
+
+      Ici {{currentDocument.count_completed_paragraphs + 1}} / {{currentDocument.count_pending_paragraphs + currentDocument.count_completed_paragraphs}} signifie que c’est le <span v-if="currentDocument.count_completed_paragraphs == 1">premier paragraphe</span><span v-else>paragraphe numéro {{currentDocument.count_completed_paragraphs}} </span> de l’article sur {{currentDocument.count_pending_paragraphs + currentDocument.count_completed_paragraphs}}.
+
+      <br><br>
+      <strong>Combien faut il de questions par paragraphe ?</strong><br>
+      Pour chaque paragraphe, il faudra créer 5 couples de “question-réponse”. Ainsi, pour l’ensemble de l’article il vous faudra faire 5 questions-réponses * 5 paragraphes = 25 questions-réponses.
+
+
+      <strong>Mais alors faut-il que je fasse 25 questions réponses?</strong><br>
+      Pas d’inquiétude ! La seule règle est de compléter un paragraphe soit 5 questions réponses. Si vous vous arrêtez avant, nous ne pourrons malheureusement pas enregistrer vos annotations. Une fois validée, vous pourrez repartir pour un nouveau paragraphe !
+      <br><br>
+
+
+      <strong>J'ai déjà eu "{{currentDocument.title}}", est-ce normal ?</strong><br>
+      Cette page Wikipedia était trop longue pour tenir en 5 paragraphes. On a donc créé <span v-if="currentDocumentChapitresInfo.total == 1"> un article</span><span v-else> {{currentDocumentChapitresInfo.total}} articles</span>  portant le même titre, mais avec les textes suivants (vous en êtes au numéro {{currentDocumentChapitresInfo.total - currentDocumentChapitresInfo.toDo + 1}}).
+      <br><br>
     </span>
   </v-tooltip>
 </template>
